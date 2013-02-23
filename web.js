fc4acaf9811b082c935e4df6d91d8ddd272d9688
Justin Patriquin <justinpatriquin@gmail.com>
2013-02-23 14:45:17 -0400

    first version of ping module

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
-
 var app = express.createServer();
 var io = require('socket.io').listen(app);
+var ping = require('./lib/ping.js');
 
 app.configure(function() {
   app.set('views', __dirname + "/views");
@@ -23,10 +23,23 @@ app.listen(port, function() {
   console.log('listening on',port);
 });
 
-
 io.sockets.on('connection', function(socket) {
   socket.emit('evt', { hello: 'world' });
   socket.on('cli', function(data) {
     console.log('data',data);
   });
+});
+
+setTimeout(ping.ping, 100);
+ping.serviceEvents.on('serviceConnect', function(){
+  console.log('Service is UP!');
+});
+
+ping.serviceEvents.on('serviceDisconnect', function(error, response){
+  if(!error){
+    console.log('Service is down! Error:', response.statusCode);
+  }
+  else{
+    console.log('Service is down!');
+  }
 });
