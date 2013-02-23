447aa4efbf739f2bf72f2ac9b925f523b99738d0
Benjamin DeCoste <bendecoste@gmail.com>
2013-02-23 13:17:41 -0400

    serve views

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req,res) {
-  res.send('hello world');
+app.configure(function() {
+  app.engine('.html', require('ejs').renderFile);
 });
+
+app.get('/', function(req,res) {
+  console.log('rendering');
+  res.render('index.html');
+});
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
